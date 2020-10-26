3feb36e1db38e73b59a5ad5f6207e3a08a88f592
hyt <yxh589@case.edu>
2020-10-26 14:11:25 +0800

    documentations

--- src/Core/Layer.hpp
+++ src/Core/Layer.hpp
@@ -6,16 +6,44 @@
 namespace HYT
 {
     /**
-     * @brief 
+     * @brief An execution layer of the application
      * 
      */
     class Layer
     {
     public:
+        /**
+         * @brief a layer is initialized when it is pushed into the stack
+         * 
+         */
         virtual void init() {};
+
+        /**
+         * @brief actions to perform during each frame
+         * 
+         * @param seconds time in seconds from last frame
+         */
         virtual void onUpdate(float seconds) {};
+
+        /**
+         * @brief a layer is terminated when it is poped from the stack
+         * 
+         */
         virtual void terminate() {};
+
+        /**
+         * @brief actions to perform when an event has occured
+         * 
+         * @param e 
+         */
         virtual void onEvent(Event &e) {};
+
+        /**
+         * @brief subscribe to a particular type of event
+         * 
+         * @param type the event type to subscribe to
+         * @param callback the function to call when an event of subscribed type has occured
+         */
         inline void subscribe(EventType type, std::function<bool(Event&)> callback)
         {
             m_eventDispatcher.subscribe(type, callback);
